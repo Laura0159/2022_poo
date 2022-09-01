829600802a39c331ff2ab843ff7cabe18cec0462
Thauane Laura <thauanecarvalho1@gmail.com>
2022-09-01 18:00:08 -0300

    Primeiro teste concluido com sucesso

--- Empregado.java
+++ Empregado.java
@@ -10,7 +10,7 @@ public class Empregado{
         if (tipo == 1)
         return salario;
         if(tipo ==2 )
-        return salario + salario + comissao;
+        return salario + salario * comissao;
         if(tipo == 3)
         return salario + bonus;
         return 0;
@@ -31,5 +31,36 @@ public class Empregado{
     public void setNome(String nome) {
         this.nome = nome;
     }
+
+    public int getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+
+    public double getBonus() {
+        return bonus;
+    }
+
+    public double getComissao() {
+        return comissao;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setBonus(double bonus) {
+        this.bonus = bonus;
+    }
+    public void setComissao(double comissao) {
+        this.comissao = comissao;
+    }
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
 
 }
